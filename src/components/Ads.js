2990e780bf809802b263602f4369a52bf02076e8
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2023-10-25 23:52:54 +0600

    push

--- src/components/Ads.js
+++ src/components/Ads.js
@@ -9,12 +9,12 @@ const Ads = () => {
   return (
     <Container className="mt-3 lg:mt-32">
       <div className="bg-[#F3F3F3] lg:flex lg:py-7 lg:pl-16">
-        <div className=" lg:w-2/4">
+        <div className=" lg:w-2/4 flex lastDevice:max-lg:justify-center">
           <Image imgsrc={"assets/watch.png"} />
         </div>
-        <div className=" lg:w-2/4 lg:py-14 py-4 lastDevice:max-lg:text-center">
+        <div className=" lg:w-2/4 lg:py-14 p-4 lastDevice:max-lg:text-center">
           <Heading title={"Watch of the year"} />
-          <p className="lg:w-[511px] font-dm text-base text-cdrop mt-9 mb-9 lg:mb-12">
+          <p className="lg:max-w-[511px] font-dm text-base text-cdrop mt-9 mb-9 lg:mb-12">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry orem Ipsum..
           </p>
